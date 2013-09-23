373d9b7df30474c52686ba65548e3894f5980b51
Dumitru Ursu <dima@ceata.org>
2013-09-23 06:41:22 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Symbiosis::Application.config.secret_key_base = '4198616d62e3244a9bd76f4bf1cc972b3216bbe1fcd198aad6c59233b02369333f61052877e44f2102976d6b1ea57861186a355f4fdefd1a83be656392b82190'
