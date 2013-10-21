ea54b668512fcb2dd2e5f94afe4c405447d8ce3e
Dumitru Ursu <dima@ceata.org>
2013-10-21 09:39:56 +0300

    Use all Zepto components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require zepto
+//= require zepto/all
 //= require rails/method
 //= require rails/confirm
 //= require rails/disable
