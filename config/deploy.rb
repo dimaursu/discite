03575c7fdf7f11aee3c59e1d7c4fc2fbca946adc
Dumitru Ursu <dima@ceata.org>
2013-10-31 18:16:54 +0200

    Add capistrano deploy script

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+set :application, 'Discite'
+set :repo_url, 'git@gitorious.org:discite/discite.git'
+
+ask :branch, :master
+
+set :deploy_to, '/home/discite/discite/'
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :default_env, 'production'
+set :keep_releases, 2
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
