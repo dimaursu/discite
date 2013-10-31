1da98b139e9c858fbcc2971d25928442b86e5c1f
Dumitru Ursu <dima@ceata.org>
2013-10-31 18:18:35 +0200

    Remove json api from courses
    We will not provide an API with this application. If we will ever do it, it will
    be a separate app.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,63 +2,46 @@ class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /courses
-  # GET /courses.json
   def index
     @courses = Course.all
   end
 
-  # GET /courses/1
-  # GET /courses/1.json
   def show
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
   def edit
   end
 
-  # POST /courses
-  # POST /courses.json
   def create
     @course = Course.new(course_params)
 
     respond_to do |format|
       if @course.save
         format.html { redirect_to @course, notice: 'Course was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @course }
       else
         format.html { render action: 'new' }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /courses/1
-  # PATCH/PUT /courses/1.json
   def update
     respond_to do |format|
       if @course.update(course_params)
         format.html { redirect_to @course, notice: 'Course was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @course.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /courses/1
-  # DELETE /courses/1.json
   def destroy
     @course.destroy
     respond_to do |format|
       format.html { redirect_to courses_url }
-      format.json { head :no_content }
     end
   end
 
@@ -70,6 +53,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:title, :description, :user_id, :language, :prerequisites, :duration)
+      params.require(:course).permit(:title, :description, :user_id, :language, :prerequisites, :duration, :rating)
     end
 end
