c1ae640934cd3aa095658f972d76e5f3f74cb983
Dumitru Ursu <dima@ceata.org>
2013-11-01 23:35:02 +0200

    Tweak capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,21 +2,11 @@ set :application, 'Discite'
 set :repo_url, 'git://gitorious.org/discite/discite.git'
 
 set :branch, :master
-set :stage, :production
 
 set :deploy_to, '/home/discite/discite/'
-set :scm, :git
-set :user, "discite"
-set :ssh_options, { :forward_agent => true }
-set :use_sudo, false
 
-set :format, :pretty
-set :log_level, :debug
-set :pty, true
-
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :rvm_ruby_version, 'rubinius'
+set :rvm_type, :user
 
 set :keep_releases, 2
 
-set :app_server, :puma
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,40 @@ set :stage, :production
 # unless any hosts have the primary property set.
 role :all, %w{discite@alt.ceata.org}
 
+puma_sock = "unix://#{shared_path}/sockets/puma.sock"
+puma_control = "unix://#{shared_path}/sockets/pumactl.sock"
+puma_state = "#{shared_path}/sockets/puma.state"
+puma_log = "#{shared_path}/log/puma-#{fetch(:stage)}.log"
+
+namespace :deploy do
+  desc "Start the application"
+  task :start do
+    on roles(:all) do
+      execute "cd #{current_path} && RAILS_ENV=#{fetch(:stage)} && bundle exec puma -b '#{puma_sock}' -e #{fetch(:stage)} -t2:4 --control '#{puma_control}' -S #{puma_state} >> #{puma_log} 2>&1 &", :pty => false
+    end
+  end
+
+  desc "Stop the application"
+  task :stop do
+    on roles(:all) do
+      execute "cd #{current_path} && RAILS_ENV=#{fetch(:stage)} && bundle exec pumactl -S #{puma_state} stop"
+    end
+  end
+
+  desc "Restart the application"
+  task :restart do
+    on roles(:all) do
+      execute "cd #{current_path} && RAILS_ENV=#{fetch(:stage)} && bundle exec pumactl -S #{puma_state} restart"
+    end
+  end
+
+  desc "Status of the application"
+  task :status do
+    on roles(:all) do
+      execute "cd #{current_path} && RAILS_ENV=#{fetch(:stage)} && bundle exec pumactl -S #{puma_state} stats"
+    end
+  end
+end
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server
