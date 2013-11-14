eca6f3d258ff8a91f4d9cddd15e65160124187a5
Dumitru Ursu <dima@ceata.org>
2013-11-14 21:54:26 +0200

    Add raleway webfont

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Discite::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
