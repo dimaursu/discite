010cdfea8addf7ac91adba3ec65af98ca65aa15b
Dumitru Ursu <dima@ceata.org>
2014-02-25 17:52:31 +0200

    Add locale switching in Profile page

--- spec/pages/profile_page.rb
+++ spec/pages/profile_page.rb
@@ -1,5 +1,19 @@
+# @author Dumitru Ursu
+# Page Object for the user profile page
 class ProfilePage
   include Capybara::DSL
+  include Warden::Test::Helpers
+  include Rails.application.routes.url_helpers
 
+  def initialize(user)
+    @user = user
+    login_as @user
+  end
+
+  def locale(lang)
+    visit edit_user_registration_path(:en)
+    select lang, from: 'user_language'
+    fill_in 'user_current_password', with: @user.password
+    click_button 'Update'
+  end
 end
-
