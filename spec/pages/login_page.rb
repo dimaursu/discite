05639907e7f5d30e56311e238c11051281f5785d
Dumitru Ursu <dima@ceata.org>
2014-02-25 17:51:44 +0200

    Add login function to Login page

--- spec/pages/login_page.rb
+++ spec/pages/login_page.rb
@@ -1,15 +1,19 @@
+# @author Dumitru Ursu
+require 'spec_helper'
+
+# This class creates a page object to the login page
 class LoginPage
   include Capybara::DSL
+  include Rails.application.routes.url_helpers
 
-  def visit_page
-    visit '/'
-    self
+  def initialize(user)
+    @user = user
+    visit new_user_session_path(:en)
   end
 
-  def login(user)
-    fill_in 'email', with: user.email
-    fill_in 'password', with: 'password'
-    click_on 'Log In'
+  def login
+    fill_in 'email', with: @user.email
+    fill_in 'password', with: @user.password
+    click_on 'Sign In'
   end
 end
-
